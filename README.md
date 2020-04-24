c8146fd20ff8b7b4ea71701bec7470a9261c6c3e
JAfory <hasanjafory@gmail.com>
2020-04-24 16:48:07 -0400

    first update

---
+++ README.md
@@ -0,0 +1,15 @@
+# JavaScript program to calculate PAMDAS and Square of two numbers
+
+#### The program will help user to calculate PANDAS and Square of two numbers by each button of operations.
+
+
+## Watch more video tutorials on youtube
+[Go here](https://www.youtube.com/channel/UCD8nhbxPg4DT0vqRxxpK6Kw/)
+
+## Demo
+[Click Here](https://jaffery97.github.com/calculator2) 
+
+### Download
+If you like to get the source code [Go Download](https://github.com/jaffery97/calculator2/archive/master.zip)
+
+#### Need Help on your assignment ? [Go here](https://edchat.info/ed).
---
+++ buildCalculator.js
@@ -0,0 +1,30 @@
+function multiplyBy() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+function sumBy() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = parseInt(num1) + parseInt(num2);
+}
+function square1By() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num1 * num1;
+}
+function subtractionBy() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = parseInt(num1) - parseInt(num2);
+}
+function divisionBy() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = parseInt(num1) / parseInt(num2);
+}
+function square2By() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = num2 * num2;
+}
